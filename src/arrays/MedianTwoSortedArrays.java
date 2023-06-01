982e3b2e73c21fe4e6e611e4f5eeda2919885c7a
vishnu <vishnu.vuyyur@gmail.com>
2023-06-01 14:06:07 +0200

    new works

---
+++ src/arrays/MedianTwoSortedArrays.java
@@ -0,0 +1,51 @@
+package arrays;
+
+import java.util.Arrays;
+//Time Complexity for mergin two sorted arrays: O(N + M), space o(m+n), m, n are size of the input arrays
+//space logic: i created a new array with m+n
+public class MedianTwoSortedArrays {
+    public static void main(String[] args){
+        int[] a= new int[]{1,3,4,7};
+        int[] b= new int[]{2,5,6,8};
+        int[] c=new int[a.length+b.length];
+        int bcounter = 0;
+        int acounter = 0;
+        int ccounter = 0;
+        while(true){
+            if(bcounter >= b.length){
+                for(int i=acounter;i<a.length;i++){
+                    c[ccounter] = a[i];
+                    ccounter++;
+                }
+                break;
+            }
+            if(acounter >= a.length){
+                for(int i=bcounter;i<b.length;i++){
+                    c[ccounter] = b[i];
+                    ccounter++;
+                }
+                break;
+            }
+            if(a[acounter]<b[bcounter]){
+                c[ccounter]=a[acounter];
+                acounter++;
+            }else{
+                c[ccounter]=b[bcounter];
+                bcounter++;
+            }
+            ccounter++;
+        }
+        System.out.println(Arrays.toString(c));
+        median(c);
+    }
+
+    static void median(int[] a){
+        float median;
+            if(a.length % 2 == 0){
+                 median = (float)(a[a.length/2]+a[(a.length/2)-1])/2;
+            }else{
+                median = a[a.length/2];
+            }
+            System.out.println(median);
+    }
+}
---
+++ src/arrays/TwoSum3Some.java
@@ -0,0 +1,45 @@
+package arrays;
+
+import java.util.*;
+
+public class TwoSum3Some {
+    public static void main(String[] args) {
+      twoSum();
+      threeSum();
+    }
+
+    static void twoSum(){
+        int[] nums = new int[]{2, 4, 11, 7, 15};
+        int target = 9;
+        int sum = 0;
+        //2,4,11,7,15
+
+        for (int i = 0; i < nums.length; i++) {
+            for(int j=i+1; j<nums.length;j++) {
+                if (nums[i]+nums[j] == target) {
+                    int[] result = new int[]{i,j};
+                    System.out.println(Arrays.toString(result));
+                    break;
+                }
+            }
+        }
+    }
+
+    static void threeSum(){
+        int[] a= new int[]{-1,0,1,2,-1,-4};
+        int target = 0;
+        Set<List<Integer>> result = new HashSet<>();
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                for(int k=j+1;k<a.length;k++){
+                    if(a[i]+a[j]+a[k]==target){
+                        List<Integer> combination = new ArrayList<>(List.of(a[i], a[j], a[k]));
+                        Collections.sort(combination);
+                        result.add(combination);
+                    }
+                }
+            }
+        }
+        result.stream().forEach(listItem->System.out.println(listItem));
+    }
+}
