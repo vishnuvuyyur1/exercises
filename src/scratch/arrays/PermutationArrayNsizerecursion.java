d8c1085cccc59eec8343e1e90d184e36b90e54da
vishnu <vishnu.vuyyur@gmail.com>
2023-06-14 14:09:32 +0200

    code changes

--- src/scratch/arrays/PermutationArrayNsizerecursion.java
+++ src/scratch/arrays/PermutationArrayNsizerecursion.java
@@ -1,28 +1,50 @@
 package scratch.arrays;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+/**
+ * Time and space complexity: O(n * n!) time | O(n!) space
+ * for a list of size n, There n! permutations.
+ * There are N! permutations and it requires O(N) time to print a permutation. timecomplexity o(n*n!)
+ * Space Complexity: O(N). Recursion stack.
+ * For each permutation,  permutations() method gets calls once so n!, and it requires o(n)
+ * time to loop throuh all the elements of the array and each time its calling permutations() method.
+   we use a for loop and we are calling permutations() inside for loop, so looping through all
+ the items in array o(n), now until the  base case is hit the permutation() dosent terminate and
+ there are n! base cases so total o(n*n!).  Therefore, there will be no more than (n * n!) function calls.
+ */
 
 public class PermutationArrayNsizerecursion {
+    static int counter;
+    static Set<List<Integer>> list;
+
 public static void main(String[] args){
-    int[] nums = new int[]{1,2,3,4};
+    int[] nums = new int[]{1,1,2};
     permute(nums);
 
 }
-    public static List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
+    public static Set<List<Integer>> permute(int[] nums) {
+        list = new HashSet<>();
         permutations(nums, 0);
+        System.out.println(counter);
+        System.out.println(list);
+
         return list;
     }
 
     private static void permutations(int[] nums, int i) {
         //if i reaches length just add nums to list and return
+        counter++;
         if (i == nums.length) {
             List<Integer> list1 = new ArrayList<>();
             for (int item : nums) {
                 list1.add(item);
             }
             System.out.println(list1);
+            list.add(list1);
             return;
         }
 
