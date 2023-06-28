9a2c7d8e9a8ae6b51a4c0643f7a1b68c3968c292
vishnu <vishnu.vuyyur@gmail.com>
2023-06-28 11:10:41 +0200

    new changes

--- src/TopicsCovered.java
+++ src/TopicsCovered.java
@@ -12,4 +12,39 @@ public class TopicsCovered {
     // inset, remove, getrandom in o(1) time
     //neighbors
     // word build
+
+    /**
+     * Types of for loops
+     *      //Bubble sort
+     *      for(int i=0;i<arry.length-1;i++){
+     *          for(int j=0;j<array.length-1-i;j++)
+     *              if(arry[j]>arry[j+1])
+     *      }
+     *      //Two sum, 3 sum, Longest palindrome
+     *      for(int i=0;i<arry.lenght;i++){
+     *          for(int j=i+1;j<arry.length;j++)
+     *              if(arry[i]+arry[j]=target)
+     *      }
+     *      //Neighbors
+     *      for(int i=0;i<arry.length;i++){
+     *          for(int j=0;j<arry.length;j++){
+     *              if(i=j) continue;
+     *      }
+     *      //Permutations
+     *      //int pos = 0;
+     *      for(int j=pos;j<arry.length;j++){
+     *          permutation(pos+1, arry);
+     *      }
+     *      //Dupes from sorted array or uniques in sorted array
+     *      int i=0;
+     *      for(int j=1;j<arry.length;j++){
+     *          if(arry[i]!=arry[j]){
+     *              i++;
+     *              arry[i]=arry[j];
+     *          }
+     *      }
+     *      //Palindrome, reverse integer
+     *      for(int i=0;i<arry.lenght/2;i++){}
+     *
+     */
 }
